6cdcf4499fd083382741bd185e812f5f5cc2fa5b
Hima Teja Cherukumalli <himatejacherukumalli0@gmail.com>
2025-11-06 06:45:06 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyB9W1EQnfTx1qcdMH9aaw5Sse6DYVUb1Ac",
-  authDomain: "6000-firebase-studio-1761666931236.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev",
+  authDomain: "studio-945841177-665ee.firebaseapp.com",
   projectId: "studio-945841177-665ee",
   storageBucket: "studio-945841177-665ee.appspot.com",
   messagingSenderId: "466253670945",
